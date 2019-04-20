7555577b79ecb0378d923edbdc285db1af118b06
aodem <43295961+aodem@users.noreply.github.com>
2019-04-20 12:50:33 -0700

    Updates

---
+++ precache-manifest.88ee5d949ed09b2bc3c9d30bab4160b1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "426efc452bc626f51c71",
+    "url": "/Memory-Game-React/static/js/runtime~main.c4bb54b9.js"
+  },
+  {
+    "revision": "743a2f2fd4bc8c41d2e3",
+    "url": "/Memory-Game-React/static/js/main.02e0af44.chunk.js"
+  },
+  {
+    "revision": "c4427d1629e59034a048",
+    "url": "/Memory-Game-React/static/js/2.45cfc2fb.chunk.js"
+  },
+  {
+    "revision": "743a2f2fd4bc8c41d2e3",
+    "url": "/Memory-Game-React/static/css/main.3811c7aa.chunk.css"
+  },
+  {
+    "revision": "0d8873c09dc45ccd2884f60be2eff45d",
+    "url": "/Memory-Game-React/index.html"
+  }
+];
